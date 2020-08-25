0a10a70a92d229e57721de2cf63908e9fa911517
Mixail <mixail.korchikov@mail.ru>
2020-08-25 16:32:30 +0300

    editor upd

--- Assets/Editor/CreateAsset.cs
+++ Assets/Editor/CreateAsset.cs
@@ -1,29 +1,48 @@
-﻿using UnityEditor;
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
 
 public class CreateAsset
 {
+    private static string mainPath = "Assets/Asset/";
 
     [MenuItem("Assets Bundle/Build AssetBundle Windows64")]
     static void BildAssetBundles()
     {
-        BuildPipeline.BuildAssetBundles("Assets/Asset", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        string directory = mainPath + "Windows64";
+        checkDir(directory);
+        BuildPipeline.BuildAssetBundles(mainPath + "Windows64", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
     }
 
     [MenuItem("Assets Bundle/Build AssetBundle Windows32")]
     static void BildAssetBundles32()
     {
-        BuildPipeline.BuildAssetBundles("Assets/Asset", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        string directory = mainPath + "Windows32";
+        checkDir(directory);
+        BuildPipeline.BuildAssetBundles(mainPath + "Windows32", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
     }
     
     [MenuItem("Assets Bundle/Build AssetBundle WebGL")]
     static void BildAssetBundlesWeb()
     {
-        BuildPipeline.BuildAssetBundles("Assets/Asset", BuildAssetBundleOptions.None, BuildTarget.WebGL);
+        string directory = mainPath + "WebGL";
+        checkDir(directory);
+        BuildPipeline.BuildAssetBundles(mainPath + "WebGL", BuildAssetBundleOptions.None, BuildTarget.WebGL);
     }
 
     [MenuItem("Assets Bundle/Build AssetBundle WSA (Hololens)")]
     static void BildAssetBundlesWsa()
     {
-        BuildPipeline.BuildAssetBundles("Assets/Asset", BuildAssetBundleOptions.None, BuildTarget.WSAPlayer);
+        string directory = mainPath + "Hololens_(WSA)";
+        checkDir(directory);
+        BuildPipeline.BuildAssetBundles(mainPath + "Hololens_(WSA)", BuildAssetBundleOptions.None, BuildTarget.WSAPlayer);
+    }
+
+    private static void checkDir(string dir)
+    {
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
     }
 }
